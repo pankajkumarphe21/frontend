41625d94a509189a24d78bd73383f93f67744aaf
Pankaj <pankaj.kumar.phe21@itbhu.ac.in>
2024-06-13 09:55:08 +0530

    final

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
     proxy:{
-      '/api':`https://server-omega-blond-77.vercel.app`,
+      '/api':`${process.env.BACKEND_URL}`,
     }
   },
   plugins: [react()],
